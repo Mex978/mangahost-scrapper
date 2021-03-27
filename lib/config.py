dd1e88995658cb6b817eb65eff06d0b12f08132d
Max Lima <max.lima2@gmail.com>
2021-03-27 20:31:07 -0300

    fix on config file

--- lib/config.py
+++ lib/config.py
@@ -3,6 +3,6 @@ import os
 TOKEN = os.getenv("TOKEN")
 GCF_URL = os.getenv("GCF_URL")
 
-BASE_URL = os.get("BASE_URL")
+BASE_URL = os.getenv("BASE_URL")
 MANGAS_URL = f"{BASE_URL}/api/v1/mangas/"
 CREATE_CHAPTER_URL = f"{BASE_URL}/api/v1/create-mangachapter/"
