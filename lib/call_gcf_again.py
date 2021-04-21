bb31ee31b29fb1531c9b5b2e84e18d0f901b78ed
Max Lima <max.lima2@gmail.com>
2021-04-21 19:26:10 -0300

    remove unecessary line

--- lib/call_gcf_again.py
+++ lib/call_gcf_again.py
@@ -5,8 +5,6 @@ import requests
 def call():
     try:
         target_audience = config.GCF_URL
-        print(target_audience)
-
         requests.post(target_audience, timeout=5)
     except:
         pass
